91f6a0853fe16b8d4b0e4b310c20b5ecb659c547
Pierson Campbell <pson.campbell@gmail.com>
2014-01-21 14:09:43 -0500

    latest revision with good progress on goals, text layers and css

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 python-psd-utils
 ================
 
-Wrapper utilities for psd-tools python package. The goal is to create some usable CSS from PSD files.
+Python parsing wrapper utilities for ruby psd gem and in the future other python PSd packages.
+
+The goal is to save a ton of time and create some usable HTML and CSS from PSD files.
+
+Currently this will give you HTML divs with all the text layers and CSS that goes with them.
+
+In the next revision I'm hoping it will also provide where it can dimension CSS for the divs in
+case its helpful for saving more time.
+
+I would like this to be pure ruby but I know python regex so for now its both ruby and python.
+
+You will need at the very least:  gem install psd   and possibly gem install psd-enginedata and of course python.
+
+The psd-parse.rb script simply runs psd with a full path to the PSD and returns the parsed data.
+
+The psd_parse.py script takes 2 arguments, the full path to the PSD and an output type ( html or css ) for example:
+
+python psd_parse.py /full/path/to/my.psd html
+
+will output divs for each layer and if the layer has text data, text divs.
+
+python psd_parse.py /full/path/to/my.psd css
+
+will output CSS for those divs
+
+I hope this saves you some time :D
+
+-C
--- psd_parse.py
+++ psd_parse.py
@@ -1,10 +1,10 @@
 #!/usr/local/bin/python
 
-import subprocess,sys,pprint,cgi,string
+import subprocess, sys, pprint, cgi, string, re
 from psd_tools import PSDImage
 
-if len(sys.argv) < 2:
-	print 'input PSD file and output file name are required... exiting'
+if len(sys.argv) < 3:
+	print 'input PSD file and output type (html or css) are required... exiting'
 	exit()
 
 """
@@ -13,65 +13,156 @@ Call ruby command to get JSON representation of PSD file
 """
 
 ruby_cmd = '/Users/pierson/2014/github/cpcbell/python-psd-utils/psd-parse.rb'
-output = subprocess.check_output([ ruby_cmd, sys.argv[1] ])
-
+output = subprocess.check_output([ruby_cmd, sys.argv[1]])
 
 layer_ary = output.split('{:type=>:layer,')
 
+"""
 pprint.pprint(layer_ary)
 exit()
-
 text_ary = []
 font_ary = []
 css_ary = []
+"""
+
+
+def css_display(layer,layer_ct):
+
+	print '.layer' + str(layer_ct) + '{'
+
+	name_re = re.compile(r', \:name\=>\"(.*?)\", ')
+	try:
+		print "\t" + '/** ' + name_re.search(layer).group(1) + ' */'
+	except:
+		pass
+
+	font_family_re = re.compile(r', \:css\=>\"font-family\: (.*?)\;')
+	try:
+		fonts = font_family_re.search(layer).group(1)
+
+		fonts_ary = fonts.split(', ')
+
+		fonts_ary.remove('AdobeInvisFont')
+
+		fonts_ary.append('sans-serif')
+
+		fonts = ', '.join('"{0}"'.format(w) for w in fonts_ary)
+
+		print "\t" + 'font-family: ' + fonts + ' ;'
+
+	except:
+		pass
+
+	font_size = None
+
+	font_size_re = re.compile(r'font-size\:\s?(.*?)\;')
+	try:
+		font_size = font_size_re.search(layer).group(1)
+
+	except:
+		pass
+
+	if font_size is not None:
+
+		font_val = float(font_size[:-2])
+
+		font_em = font_val
+
+		if 'px' in font_size:
+
+			px_to_em = 16
+
+			if font_val > px_to_em:
+				font_em = font_val / px_to_em
+			else:
+				font_em = px_to_em / font_val
+
+		if 'pt' in font_size:
+
+			pt_to_em = 11.95
+
+			if font_val > pt_to_em:
+				font_em = font_val / pt_to_em
+			else:
+				font_em = pt_to_em / font_val
+
+		font_em = round(font_em, 2)
+
+		print "\t" + 'font-size: ' + str(font_em) + 'em;'
+
+
+	font_color_re = re.compile(r'\\ncolor\:\s?(.*?)\;')
+	try:
+		font_color = font_color_re.search(layer).group(1)
+
+		print "\t" + 'color: ' + font_color + ' ;'
+	except:
+		pass
+
+	print '}'
+
+	return layer
+
+def html_display(layer,layer_ct):
+
+	text_str = None
+
+	try:
+		text_re = re.compile(r':text\=>\{:value\=>\"(.*?)\",\s?')
+
+		text_str = text_re.search(layer).group(1)
+	except:
+		print '<div class="layer' + str(layer_ct) + '"></div>'
+		return None
+
+	try:
+		text_str = cgi.escape(text_str)
+	except:
+		pass
+
+	try:
+		uni_re = re.compile(r'(\\u0019)')
+
+		text_str = uni_re.sub('&#39;',text_str)
+	except:
+		pass
+
+	try:
+		quote_re = re.compile(r'(\\")')
+
+		text_str = quote_re.sub('&#34;',text_str)
+	except:
+		pass
+
+	try:
+		white_space_re = re.compile(r'(\\n)')
+
+		text_str = white_space_re.sub(' ',text_str)
+	except:
+		pass
+
+	try:
+		white_space_re = re.compile(r'(\s+)')
+
+		text_str = white_space_re.sub(' ',text_str)
+	except:
+		pass
+
+	print '<div class="layer' + str(layer_ct) + '">' + text_str + '</div>'
+
+	return layer
+
+layer_ct = 1
 
 for layer in layer_ary:
 
-	text_ary.append( layer.split(', :text=>{:value=>"') )
-	font_ary.append( layer.split(':font=>') )
-	css_ary.append( layer.split(':css=>') )
+	if sys.argv[2] == 'css':
+		css_display(layer,layer_ct)
 
-for the_text in css_ary:
-	print the_text
+	else:
+		html_display(layer,layer_ct)
 
-exit()
-
-for font in font_ary:
-
-	print font
-
-	css_ary = font.split(':css=>')
-
-	print css_ary[0]
+	layer_ct += 1
 
 exit()
 
-psd = PSDImage.load(sys.argv[1])
-
-for layer in psd.layers:
-
-	print layer.name
-
-	if layer.bbox:
-		pprint.pprint(layer.bbox)
-
-	text_str = None
-	if layer.text_data:
-
-		pprint.pprint(layer.text_data)
-
-		text_str = str(pprint.pformat(layer.text_data.text))
-
-		try:
-
-			text_str = cgi.escape(text_str).encode('ascii','xmlcharrefreplace')
-		except:
-			print 'failed to decode/encode'
-			pass
-
-		text_str = "</div><div>".join(text_str.split("\\r"))
-
-		text_str = '<div>' + text_str + '</div>'
-
-	if text_str is not None:
-		print text_str
